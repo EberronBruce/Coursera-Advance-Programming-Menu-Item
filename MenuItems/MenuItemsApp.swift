3ae3f3125db74b1693200ac969578ccdbbacd390
Bruce <bruce@redravencomputing.com>
2023-08-27 01:14:17 -0500

    Initial Commit

---
+++ MenuItems/MenuItemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuItemsApp.swift
+//  MenuItems
+//
+//  Created by Bruce Burgess on 8/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
